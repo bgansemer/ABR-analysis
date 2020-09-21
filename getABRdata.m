026e6b75614c7bd0f44cc8187945f5ccd1dfdc64
bgansemer <benjamin-gansemer@uiowa.edu>
2020-09-21 14:33:03 -0500

    updated scripts

--- getABRdata.m
+++ getABRdata.m
@@ -1,26 +1,28 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Author: Benjamin Gansemer
-%Affiliation: Green Lab, University of Iowa
-%Date Started: July 2020
-%Last Updated: August 2020
+% Author: Benjamin Gansemer
+% Affiliation: Green Lab, University of Iowa
+% Date Started: July 2020
+% Last Updated: August 2020
 
-%The function in this script is used to read 
-%    auditory brainstem response (ABR) data into Matlab.
-%The code is written for data exported in ASCII format from BioSigRZ.
-%Takes txt file as input and outputs a struct with all data from the txt
+% The function in this script is used to read 
+% auditory brainstem response (ABR) data into Matlab.
+% The code is written for data exported in ASCII format from BioSigRZ.
+% Takes txt file as input and outputs a struct with all data from the txt
 % file. Should contain animal info and table of all ABR traces where each
 % waveform dataset is a single column.
-%Additional notes here
+% Additional notes here
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function st = getABRdata(dataFile)
+% data file should be a txt file from BioSigRZ containing all records to be
+% analyzed from one animal at one timepoint. 
 %% Read in raw data and divide into individual records (traces)
 
 abrFile = fopen(dataFile, 'r');
 rawData = textscan(abrFile, '%s', 'Delimiter', '\n');
 fclose(abrFile);
 
-%split group info into separate array
+% split group info into separate array
 if contains(rawData{1}{1}, 'ABR Group Header');
     groupInfo = rawData{1}(1:13);
     noGroup = rawData{1}(14:length(rawData{1}));    
@@ -29,8 +31,8 @@ else
     data = rawData;
 end
 
-%write each record to an individual array
-numRecords = length(grep(data{1}, 'Record Number')); %get number of traces
+% write each record to an individual array
+numRecords = length(grep(data{1}, 'Record Number')); % get number of traces
 for i = 1:numRecords
     start = (length(data{1})/numRecords)*(i-1)+1;
     fin = start + 308;
@@ -43,7 +45,7 @@ for i = 1:numRecords
 %         end
 %    end
     
-    %generates nested array off all individual arrays
+    % generates nested array off all individual arrays
     combinedTable{i} = recordTable; 
     
 end
@@ -51,36 +53,36 @@ end
 %% Parse through all arrays and create a labeledSignalSet for Signal Analyzer
 % need to rewrite to put the data into a matrix or table (not LSS)
 
-% this is for a labeledSignalSet
+% OLD this is for a labeledSignalSet
 %allTraces = labeledSignalSet; %generate empty signal set to put data into
 %allTraces.Description = 'All ABR traces';
 %allTraces.TimeInformation = 'sampleRate';
 %traceNames = {};
 
-%trying to generate a struct instead
-st = struct([])
-st(1).Info = groupInfo
+% trying to generate a struct instead
+st = struct([]);
+st(1).Info = groupInfo;
 
 allWFs = cell2table({});
 
 for a = 1:length(combinedTable)
    tempTable = combinedTable{a}; %create temporary array to work with
    
-   %split the data into info about the trace and the actual waveform data
+   % split the data into info about the trace and the actual waveform data
    traceInfo = table2cell(tempTable(1:16, 'Var1'));
    traceData = tempTable(17:height(tempTable)-1, 'Var1');
    
-   %get record number of the trace
+   % get record number of the trace
    %recordCell = grep(traceInfo, 'Record');
    %record = textscan(recordCell{1}, '%s', 'Delimiter', ':');
    %rcd = record{1}{2};
    
-   %get the dB level of the trace
+   % get the dB level of the trace
    levelCell = grep(traceInfo, 'Level');
    level = textscan(levelCell{1}, '%s', 'Delimiter', '=');
    lvl = strrep(level{1}{2}, ' ', '_');
    
-   %set name of trace based on record number and dB level
+   % set name of trace based on record number and dB level
    %tName = strcat(rcd, '_', lvl);
    
    count = 0;
@@ -96,10 +98,10 @@ for a = 1:length(combinedTable)
    traceTable = array2table(str2double(traceData.Var1));
    traceTable.Properties.VariableNames{'Var1'} = lvl;
    
-   %add data to allTraces
+   % OLD add data to allTraces
    %addMembers(allTraces, traceMat, [24.41e3]);
     
-   %add data to allWFs table
+   % add data to allWFs table
    allWFs = [allWFs traceTable];
    
    
--- identifyPeaks.m
+++ identifyPeaks.m
@@ -2,7 +2,7 @@
 %Author: Benjamin Gansemer
 %Affiliation: Green Lab, University of Iowa
 %Date Started: August 2020
-%Last Updated: August 2020
+%Last Updated: September 2020
 
 %The function(s) in this script are used to identify peaks in ABR
 %waveforms. Main focus is on wave I amplitude. 
@@ -16,3 +16,38 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+function [data, peaks, crosscorrs] = identifyPeaks(dataFile)
+% function peaks = identifyPeaks(waveforms)
+% waveforms - table where each column is waveform data from a single animal,
+% each waveform is from different stimulus level. The variable name is the
+% stimulus level.
+% Do I want to have this call getABRdata? Probably.
+
+% OR - input argument is the dataFile, then first step is to run getABRdata
+% to read the data into Matlab.
+
+%% Read the raw data into Matlab using getABRdata.m and get waveform data
+data = getABRdata(dataFile);
+
+waveforms = data.Waveforms;
+
+peaks = table2array(waveforms);
+
+%% calculate signal-to-noise ratio?
+%SNR - calculate SEM of averages
+
+
+%% perform cross-correlation of each waveform with the 90dB waveform using xcorr
+
+%iterating through matrix, where c is column number
+%matrix(:, c)
+
+crosscorrs = [];
+[rws, cls] = size(peaks);
+
+for i = 1:cls
+    tempcorr = xcorr(peaks(:, i), peaks(:, 1));
+    
+    crosscorrs = [crosscorrs tempcorr];
+
+end
